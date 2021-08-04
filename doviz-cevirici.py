cbfbc29ed7c7d9c26e5f95619be671447a7227ac
Mustafa Uçar <76887611+Mstfucrr@users.noreply.github.com>
2021-08-05 01:29:53 +0300

    Update doviz-cevirici.py

--- doviz-cevirici.py
+++ doviz-cevirici.py
@@ -18,7 +18,8 @@ def indexPOST():
     d_1 = request.form.get("d-1")
     d_2 = request.form.get("d-2")
     miktar = request.form.get("miktar")
-    url = "http://data.fixer.io/api/latest?access_key=56e4f46c90223458fde9fd92e590d51e&format=1"
+    Your_access_key = ""
+    url = "http://data.fixer.io/api/latest?access_key="+Your_access_key
     response = requests.get(url)
     json_data = response.json()
     result = float(miktar) *( json_data["rates"][d_2] / json_data["rates"][d_1])
